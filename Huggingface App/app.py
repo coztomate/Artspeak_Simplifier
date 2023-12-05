100eb20d308523cd8b420c934d711ce5aba8149f
Katja Dittrich <coztomate@gmail.com>
2023-12-05 14:39:14 +0100

    20231205_kd: updated hf app

--- Huggingface App/app.py
+++ Huggingface App/app.py
@@ -2,21 +2,40 @@
 import streamlit as st
 from PIL import Image
 import io
-from transformers import pipeline
-from transformers import AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
-import torch
-from torchvision import transforms
-import open_clip
 from openai import OpenAI
-import openai
-from diffusers import StableDiffusionPipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+from huggingface_hub import InferenceClient
+client = InferenceClient()
+
+# load the simplifier model
+# Load the tokenizer and model (do this outside the function for efficiency)
+tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-small-finetuned-text-simplification")
+model = AutoModelForSeq2SeqLM.from_pretrained("mrm8488/t5-small-finetuned-text-simplification")
+
+def simplify_text(input_text):
+    # Tokenize and encode the input text
+    input_ids = tokenizer.encode("simplify: " + input_text, return_tensors="pt")
+
+    # Generate the simplified text
+    output = model.generate(input_ids, min_length=20, max_length=50, do_sample=True)
+    
+    # Decode the simplified text
+    simplified_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    # Remove or replace unwanted tokens like "SEP>"
+    cleaned_text = simplified_text.replace("SEP>", "")
+
+    return cleaned_text
+
 
 # Initialize session state variables
+if 'summary_text' not in st.session_state:
+    st.session_state['summary_text'] = ''
 if 'simplified_text' not in st.session_state:
     st.session_state['simplified_text'] = ''
 if 'new_caption' not in st.session_state:
-    st.session_state['new_caption'] = ''
+    st.session_state['new_caption'] = None
 if 'model_clip' not in st.session_state:
     st.session_state['model_clip'] = None
 if 'transform_clip' not in st.session_state:
@@ -33,62 +52,9 @@ if 'image_from_simplified_text' not in st.session_state:
     st.session_state['image_from_simplified_text'] = None
 if 'image_from_press_text' not in st.session_state:
     st.session_state['image_from_press_text'] = None
+if 'image_from_press_text_from_caption' not in st.session_state:
+    st.session_state['image_from_press_text_from_caption'] = None
 
-######loading models########
-
-####loading simplifier model#####
-# Define model and tokenizer names for the text simplification model
-model_name = "mrm8488/t5-small-finetuned-text-simplification"
-tokenizer_name = "mrm8488/t5-small-finetuned-text-simplification"
-
-# Load models only once in session state
-if 'model' not in st.session_state or 'tokenizer' not in st.session_state:
-    st.session_state['model'] = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    st.session_state['tokenizer'] = AutoTokenizer.from_pretrained(tokenizer_name)
-    st.session_state['simplifier'] = pipeline("text2text-generation", model=st.session_state['model'], tokenizer=st.session_state['tokenizer'])
-
-# Use the model from session state
-simplifier = st.session_state['simplifier']
-
-####loading clip model#####
-# Function to load the CLIP model
-def load_clip_model():
-    model_clip, _, transform_clip = open_clip.create_model_and_transforms(
-        model_name="coca_ViT-L-14",
-        pretrained="mscoco_finetuned_laion2B-s13B-b90k"
-    )
-    return model_clip, transform_clip
-
-if 'loaded_clip_model' not in st.session_state or 'loaded_transform_clip' not in st.session_state:
-    st.session_state['loaded_clip_model'], st.session_state['loaded_transform_clip'] = load_clip_model()
-
-# Function to generate a caption using the preloaded CLIP model
-def generate_caption(image_path):
-    im = Image.open(image_path).convert("RGB")
-    im = st.session_state['loaded_transform_clip'](im).unsqueeze(0)
-
-    # Generate a caption for the image
-    with torch.no_grad(), torch.cuda.amp.autocast():
-        generated = st.session_state['loaded_clip_model'].generate(im)
-
-    new_caption = open_clip.decode(generated[0]).split("<end_of_text>")[0].replace("<start_of_text>", "")[:-2]
-    return new_caption
-
-###loading diffusion model
-# Function to load the Stable Diffusion model
-def load_diffusion_model():
-    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-    pipe = pipe.to("cuda")
-    return pipe
-
-# Initialize the model at the start and store it in the session state
-if 'loaded_model' not in st.session_state:
-    st.session_state['loaded_model'] = load_diffusion_model()
-
-# Function to generate an image using the preloaded model
-def generate_image(prompt):
-    image = st.session_state['loaded_model'](prompt).images[0]
-    return image
 
 ################################################
 
@@ -122,29 +88,50 @@ with st.expander("Display of Uploaded Files"):
 st.markdown("---")
 
 ####Get summary
+if st.button("Summarize"):
+    if user_input:
+        summarized_text = client.summarization(user_input)
+        st.session_state['summary_text'] = summarized_text
+    else:
+        st.warning("Please enter text in the input field before clicking 'Save'")
+
+# Display the simplified text from session state
+if st.session_state['summary_text']:
+    st.write("Summary Original Text:")
+    st.write(st.session_state['summary_text'])
+
+st.markdown("---")
+
 if st.button("Simplify"):
     if user_input:
-        simplified_text = simplifier(user_input, min_length=20, max_length=50, do_sample=True)
-        # Update the session state
-        st.session_state['simplified_text'] = simplified_text[0]['generated_text']
+        simplified_text = simplify_text(user_input)
+        st.session_state['simplified_text'] = simplified_text
     else:
         st.warning("Please enter text in the input field before clicking 'Save'")
 
 # Display the simplified text from session state
 if st.session_state['simplified_text']:
-    st.write("Simplified Original Text:")
+    st.write("Simplify Original Text:")
     st.write(st.session_state['simplified_text'])
 
 st.markdown("---")
 
 ####Get new caption
 # Modify the 'Get Caption' button section
-if st.button("Get New Caption"):
+if st.button("Get New Caption for Uploaded Image"):
     if uploaded_image is not None:
-        # Generate the caption
-        caption = generate_caption(uploaded_image)
-        # Update the session state
-        st.session_state['new_caption'] = caption
+        try:
+            # Convert the uploaded file to bytes
+            image_bytes = uploaded_image.getvalue()
+
+            # Generate the caption (make sure to send the image in the correct format expected by your API)
+            caption = client.image_to_text(image_bytes)
+            
+            # Update the session state
+            st.session_state['new_caption'] = caption
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
     else:
         st.warning("Please upload an image before clicking 'Get Caption'")
 
@@ -174,14 +161,14 @@ def get_openai_completion(api_key, prompt_message):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "I give a short description of an artwork. Please write a complex press text for an exhibiton in international art english dealing with post-colonialism, identity politics, military industrial complex and queerness through the language of writerts like Ranciere, Deleuze, Trevor Paglen, Hito Steyerl, Slavoy Zizek, Claire Fontane, Michel Foucault, Donna Harraway and Paul Preciado. Without doing too many name drops. Just output the press text and not surrounding or explaining messages with it."},
+            {"role": "system", "content": "I give a short description of an artwork. Please create a complex exhibition press text based on the given artwork description using international art english dealing with post-colonialism, military industrial complex, anthropocene, identity politics and queerness through the language of Rancière, Fontane, Paglen, Deleuze, Steyerl, Spivak, Preciado, Žižek, Foucault and Harraway. Avoid excessive namedropping. Just output press text without explaining your actions."},
             {"role": "user", "content": prompt_message}
         ]
     )
     return completion.choices[0].message.content
 
 # Button to generate press text from new caption
-if st.button("Generate Press Text from New Caption"):
+if st.button("Generate Press Text from New Image Caption"):
     if st.session_state['new_caption'] and st.session_state['openai_api_key']:
         try:
             st.session_state['message_content_from_caption'] = get_openai_completion(st.session_state['openai_api_key'], st.session_state['new_caption'])
@@ -221,7 +208,7 @@ st.markdown("---")
 if st.button("Generate Image from New Caption of Artwork"):
     if st.session_state['new_caption']:
         prompt_caption = f"contemporary art of {st.session_state['new_caption']}"
-        st.session_state['image_from_caption'] = generate_image(prompt_caption)
+        st.session_state['image_from_caption'] = client.text_to_image(prompt_caption)
 
 # Display the image generated from new caption
 if st.session_state['image_from_caption'] is not None:
@@ -231,21 +218,34 @@ if st.session_state['image_from_caption'] is not None:
 if st.button("Generate Image from Simplified Text"):
     if st.session_state['simplified_text']:
         prompt_summary = f"contemporary art of {st.session_state['simplified_text']}"
-        st.session_state['image_from_simplified_text'] = generate_image(prompt_summary)
+        st.session_state['image_from_simplified_text'] = client.text_to_image(prompt_summary)
 
 # Display the image generated from simplified text
 if st.session_state['image_from_simplified_text'] is not None:
     st.image(st.session_state['image_from_simplified_text'], caption="Image from Simplified Text", use_column_width=True)
 
-# Button to generate image from press text
-if st.button("Generate Image from new Press Text"):
+# Button to generate image from press text from simplified text
+if st.button("Generate Image from new Press Text from Simplified Text"):
     if st.session_state['message_content_from_simplified_text']:
         prompt_press_text = f"contemporary art of {st.session_state['message_content_from_simplified_text']}"
-        st.session_state['image_from_press_text'] = generate_image(prompt_press_text)
+        st.session_state['image_from_press_text'] = client.text_to_image(prompt_press_text)
 
-# Display the image generated from press text
+# Display the image generated from press text from simplified text
 if st.session_state['image_from_press_text'] is not None:
-    st.image(st.session_state['image_from_press_text'], caption="Image from Press Text", use_column_width=True)
+    st.image(st.session_state['image_from_press_text'], 
+             caption="Image from Press Text from simplified Text", 
+             use_column_width=True)
 
+# Button to generate image from press text from caption
+if st.button("Generate Image from new Press Text from new Caption"):
+    if st.session_state['message_content_from_caption']:
+        prompt_press_text_caption = f"contemporary art of {st.session_state['message_content_from_caption']}"
+        st.session_state['image_from_press_text_from_caption'] = client.text_to_image(prompt_press_text_caption)
+
+# Display the image generated from press text from caption
+if st.session_state['image_from_press_text_from_caption'] is not None:
+    st.image(st.session_state['image_from_press_text_from_caption'], 
+             caption="Image from Press Text from new Caption", 
+             use_column_width=True)
 
 st.markdown("---")
